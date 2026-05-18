a9cd9567d02fc3382edb06078d9770a4c93adc23
Mel <krrishdureja96@gmail.com>
2026-05-18 14:05:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,6 @@ load_dotenv()
 
 token = os.getenv("DISCORD_TOKEN")
 
-handler = logging.FileHandler(
-    filename="discord.log",
-    encoding="utf-8",
-    mode="w"
-)
-
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
@@ -220,4 +214,4 @@ async def snipe(ctx):
 async def catch(ctx):
     await snipe(ctx)
 
-bot.run(token, log_handler=handler, log_level=logging.INFO)
+bot.run(token)
